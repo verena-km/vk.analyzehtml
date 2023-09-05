0c0900f24494d336e83e8102ed91d3663e2e3975
Verena Kunz-Meier <verena@kunz-meier.de>
2023-09-05 16:06:43 +0200

    base functionality

--- src/vk/analyzehtml/views/analyze_html_view.py
+++ src/vk/analyzehtml/views/analyze_html_view.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # from vk.analyzehtml import _
+from bs4 import BeautifulSoup
+from plone import api
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
@@ -21,4 +23,77 @@ class AnalyzeHtmlView(BrowserView):
 
     def __call__(self):
         # Implement your own actions:
+        # print("AnalyzeHtmlView Called")
+        # results = api.content.find(portal_type='Document')
+        # for result in results:
+        #     print(result.Title)
+        #     object = result.getObject()
+        #     soup = BeautifulSoup(object.text.raw, "html.parser")
+        #     for tag in soup.findAll(True):
+        #         print(tag.name)
+        # print(object.text.raw)
+        # print(object.text.output)
         return self.index()
+
+    def bla(self):
+        return "Huhu"
+
+    def tags_old(self):
+        tag_dict = {}
+        results = api.content.find(portal_type="Document")
+        for result in results:
+            # print(result.Title)
+            # print(result.Path)
+            object = result.getObject()
+            soup = BeautifulSoup(object.text.raw, "html.parser")
+            for tag in soup.findAll(True):
+                # print(tag.name)
+                if not tag.name in tag_dict.keys():
+                    tag_dict[tag.name] = {result.Title}
+                else:
+                    tag_dict[tag.name].add(result.Title)
+            print(tag_dict)
+
+            # print(object.text.raw)
+            # print(object.text.output)
+        return tag_dict
+
+    def tags(self):
+        tag_dict = {}
+        results = api.content.find(portal_type=["Document", "News Item"])
+        for result in results:
+            # print(result.Title)
+            # print(result.Path)
+            object = result.getObject()
+            soup = BeautifulSoup(object.text.raw, "html.parser")
+            for tag in soup.findAll(True):
+                # print(tag.name)
+                if not tag.name in tag_dict.keys():
+                    tag_dict[tag.name] = {object}
+                else:
+                    tag_dict[tag.name].add(object)
+            # print(tag_dict)
+
+            # print(object.text.raw)
+            # print(object.text.output)
+        return tag_dict
+
+    def classes(self):
+        class_dict = {}
+        results = api.content.find(portal_type=["Document", "News Item"])
+        for result in results:
+            object = result.getObject()
+            soup = BeautifulSoup(object.text.raw, "html.parser")
+            classes = [
+                value
+                for element in soup.find_all(class_=True)
+                for value in element["class"]
+            ]
+            print(classes)
+
+            for classname in classes:
+                if not classname in class_dict.keys():
+                    class_dict[classname] = {object}
+                else:
+                    class_dict[classname].add(object)
+        return class_dict
