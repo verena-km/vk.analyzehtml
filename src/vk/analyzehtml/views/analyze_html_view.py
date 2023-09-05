cca54cbb8a6275b9b033b3b24c8afac5baa6baec
Verena Kunz-Meier <verena@kunz-meier.de>
2023-09-05 22:12:55 +0200

    added count

--- src/vk/analyzehtml/views/analyze_html_view.py
+++ src/vk/analyzehtml/views/analyze_html_view.py
@@ -8,12 +8,8 @@ from zope.interface import implementer
 from zope.interface import Interface
 
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class IAnalyzeHtmlView(Interface):
     """Marker Interface for IAnalyzeHtmlView"""
-
 
 @implementer(IAnalyzeHtmlView)
 class AnalyzeHtmlView(BrowserView):
@@ -22,41 +18,7 @@ class AnalyzeHtmlView(BrowserView):
     # template = ViewPageTemplateFile('analyze_html_view.pt')
 
     def __call__(self):
-        # Implement your own actions:
-        # print("AnalyzeHtmlView Called")
-        # results = api.content.find(portal_type='Document')
-        # for result in results:
-        #     print(result.Title)
-        #     object = result.getObject()
-        #     soup = BeautifulSoup(object.text.raw, "html.parser")
-        #     for tag in soup.findAll(True):
-        #         print(tag.name)
-        # print(object.text.raw)
-        # print(object.text.output)
         return self.index()
-
-    def bla(self):
-        return "Huhu"
-
-    def tags_old(self):
-        tag_dict = {}
-        results = api.content.find(portal_type="Document")
-        for result in results:
-            # print(result.Title)
-            # print(result.Path)
-            object = result.getObject()
-            soup = BeautifulSoup(object.text.raw, "html.parser")
-            for tag in soup.findAll(True):
-                # print(tag.name)
-                if not tag.name in tag_dict.keys():
-                    tag_dict[tag.name] = {result.Title}
-                else:
-                    tag_dict[tag.name].add(result.Title)
-            print(tag_dict)
-
-            # print(object.text.raw)
-            # print(object.text.output)
-        return tag_dict
 
     def tags(self):
         tag_dict = {}
@@ -76,6 +38,7 @@ class AnalyzeHtmlView(BrowserView):
 
             # print(object.text.raw)
             # print(object.text.output)
+        #print(tag_dict)
         return tag_dict
 
     def classes(self):
@@ -89,11 +52,12 @@ class AnalyzeHtmlView(BrowserView):
                 for element in soup.find_all(class_=True)
                 for value in element["class"]
             ]
-            print(classes)
+            # print(classes)
 
             for classname in classes:
                 if not classname in class_dict.keys():
                     class_dict[classname] = {object}
                 else:
                     class_dict[classname].add(object)
+        #print(class_dict)
         return class_dict
